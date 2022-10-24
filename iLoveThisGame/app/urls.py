afb9cc0e9fb7d91c7a03568de4f656928922119a
Graciela Romano <graci.romano@gmail.com>
2022-10-24 10:43:05 -0300

    Creacion de empresa, se agrego el carrito en la barra de navegacion y el icono

--- iLoveThisGame/app/urls.py
+++ iLoveThisGame/app/urls.py
@@ -1,7 +1,10 @@
-from django.urls import path
+from django.urls import path, re_path
 from . import views
 
 urlpatterns = [
     path('', views.home, name='home'),
     path('contacto', views.contact, name='contact'),
+    path('empresa', views.empresa, name='empresa'),
+    # /categoria/calzados-deportivos
+    re_path(r'^categoria/(?P<name>[-\w]+)$', views.category, name='categoria'),
 ]
--- iLoveThisGame/app/views.py
+++ iLoveThisGame/app/views.py
@@ -7,3 +7,11 @@ def contact(request):
 def home(request):
     return render(request, 'app/pages/index.html', {})
 
+def empresa(request):
+    return render(request,'app/pages/empresa.html', {})
+
+def category(request,name):
+    products = {name:'zapatila azul'}
+    return render(request,'app/pages/categoria.html', products)
+
+
