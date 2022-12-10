8c76480325be63c784a15a2c591ceaa038a84238
AdriGimenez <93161937+AdriGimenez@users.noreply.github.com>
2022-12-10 14:40:25 -0500

    se agrego modelos, forms, urls y una carpeta administracion en template y static

--- iLoveThisGame/app/models.py
+++ iLoveThisGame/app/models.py
@@ -1,3 +1,25 @@
 from django.db import models
 
-# Create your models here.
+
+class Marca(models.Model):
+    nombre = models.CharField( max_length=20, verbose_name='Marca')
+
+    def __str__(self):
+        return self.nombre
+
+class Categoria (models.Model):
+    nombre = models.CharField(max_length=20, verbose_name='Categoria')
+
+    def __str__(self):
+        return self.nombre
+
+class Articulo (models.Model):
+    nombre = models.CharField(max_length=50, verbose_name='Articulo')
+    descripcion = models.TextField(max_length=250, null=True, blank=True, verbose_name='Descripcion')
+    precio = models.PositiveIntegerField(verbose_name='Precio')
+    cantidad = models.PositiveIntegerField(verbose_name='Cantidad')
+    colorPrincipal = models.CharField(max_length=10)
+    talla = models.CharField(max_length=10)
+    marca = models.ForeignKey(Marca, on_delete=models.CASCADE)
+    categoria = models.ForeignKey(Categoria, on_delete=models.CASCADE)
+    
--- iLoveThisGame/iLoveThisGame/settings.py
+++ iLoveThisGame/iLoveThisGame/settings.py
@@ -76,8 +76,12 @@ WSGI_APPLICATION = 'iLoveThisGame.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'tienda_db',
+        'USER': 'postgres',
+        'PASSWORD': 'root1511',
+        'HOST': '127.0.0.1',
+        'PORT': '5432',
     }
 }
 
