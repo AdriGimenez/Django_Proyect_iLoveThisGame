eec027bc76352ae0e8b0e3196c13adbac86ae879
Graciela Romano <graci.romano@gmail.com>
2022-12-14 10:58:32 -0300

    creacion de login y registrarse, validados

--- iLoveThisGame/app/views.py
+++ iLoveThisGame/app/views.py
@@ -2,13 +2,14 @@ from django.shortcuts import render, redirect
 from django.contrib import messages
 from .models import Articulo, Marca, Categoria
 from .forms import ArticuloForm, MarcaForm, CategoriaForm
-
 from django.views import View
 from django.views.generic import ListView
+from app.forms import ContactoForm, RegistrarUsuarioForm, BuscarForm
+from django.contrib.auth import authenticate, login
+from django.contrib.auth.forms import AuthenticationForm
+from django.contrib.auth.decorators import login_required
 
-from app.forms import ContactoForm
-
-
+# Vistas publicas
 def contact(request):
     if request.method =="POST":
         contacto_form = ContactoForm(request.POST)
@@ -20,8 +21,22 @@ def contact(request):
         contacto_form = ContactoForm()
     return render(request, 'app/pages/contact.html', {'contacto_form':contacto_form})
 
+def buscar(request):
+    if request.method =="POST":
+        buscar_form = BuscarForm(request.POST)
+        texto = request.POST.get("cadenabuscar")
+
+        ## Mostramos una lista de coincidencias
+        return render(request,'app/pages/buscar.html', {'cadenabuscar': texto})
+
+    else:
+        buscar_form = BuscarForm()
+
+    return render(request,'app/pages/buscar.html', {})
+
 def home(request):
-    return render(request, 'app/pages/index.html', {})
+    buscar_form = BuscarForm()
+    return render(request, 'app/pages/index.html', {'buscar_form':buscar_form})
 
 def empresa(request):
     return render(request,'app/pages/empresa.html', {})
@@ -37,9 +52,48 @@ def descripcion(request, producto):
 def oferta(request):
     return render(request,'app/pages/oferta.html', {})
 
+def perfil(request):
+    return render(request,'app/pages/perfil.html', {})
+
+### Vistas Protegidas.
 def index_administracion(request):
     variable = 'test variable'
     return render(request, 'app/administracion/index_administracion.html', {'variable': variable})
+
+#Login y registracion de nuevos usuarios.
+def login(request):
+    return render(request,'app/registration/login.html', {})
+
+def iniciar_sesion(request):
+    if request.method == 'POST':
+        # AuthenticationForm_can_also_be_used__
+        username = request.POST['username']
+        password = request.POST['password']
+        user = authenticate(request, username=username, password=password)
+        if user is not None:
+            form = login(request, user)
+            nxt = request.GET.get("next",None)
+            messages.success(request, f' Bienvenido/a {username} !!')
+            if nxt is None:
+                return redirect('home')
+            else:
+                return redirect(nxt)
+        else:
+            messages.error(request, f'Cuenta o password incorrecto, realice el login correctamente')
+    form = AuthenticationForm()
+    return render(request, 'app/registration/login.html', {'form': form})
+
+def registrarse(request):
+    if request.method == 'POST':
+        form = RegistrarUsuarioForm(request.POST)
+        if form.is_valid():
+            form.save()
+            messages.success(
+                request, f'Tu cuenta fue creada con éxito! Ya te podes loguear en el sistema.')
+            return redirect('login')
+    else:
+        form = RegistrarUsuarioForm()
+    return render(request, 'app/registration/registrarse.html', {'form': form})
 
 #Views Articulos 
 def articulos_index(request):
@@ -214,3 +268,6 @@ def categorias_eliminar(request, id_categoria):
     except ValueError as ve:
         messages.error(request=request, message="No se puede borrar")
     return redirect('categorias_index')
+
+
+
